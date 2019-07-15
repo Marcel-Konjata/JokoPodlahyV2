2bf256a5a968603bd797054e48953c0edba499cc
Marcel Konjata <rhonin33@gmail.com>
2019-07-15 21:10:32 +0200

    customized 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,8 +7,20 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div style={{
+      textAlign:"center",
+      fontFamily:'Montserrat',
+      paddingTop:'20%',
+      paddingBottom: '10%'
+    }} >
+
+    <h1 style={{marginBottom:25}}>Stránka nenalezena</h1>
+    <p>zadali jste adresu do prohlížeče, která nevede na správnou cestu</p>
+    <p>zkontrolujte si adresu v horním řádku</p>
+    <p>pokud jste napsali jokopodlahy.cz a pořád se vám objevuje tento text je něco špatně na straně serveru <br/>
+    a podnikneme vše pro znovu zprovozění stránek, děkujeme za pochopení
+    </p>
+    </div>
   </Layout>
 )
 
